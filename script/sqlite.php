b2cc53a616c96c3baee70be01870aa7dc81707c7
adlaternae <92540154+adlaternae@users.noreply.github.com>
2022-04-03 19:14:34 +0300

    Update sqlite.php

--- script/sqlite.php
+++ script/sqlite.php
@@ -5,6 +5,26 @@ $db = new SQLite3('catalog.sqlite');
 
 $db->createFunction('lower', 'mb_strtolower');
 $db->createFunction('upper', 'mb_strtoupper');
+//  $db->createFunction('ltrim', 'ltrim');
+//  $db->createFunction('rtrim', 'rtrim');
+//  $db->createFunction('trim', 'trim');
+$db->createFunction('explode', 'explode');
+$db->createFunction('implode', 'implode');
+
 $db->createFunction('metaphone', 'metaphone');
 $db->createFunction('translit', 'translit');
+
+//  CREATE TABLE catalog (surname varchar(255), name varchar(255), patronymic varchar(255), title text, subtitle text, language char(2), year int, series text, id integer);
+
+$db->exec('CREATE INDEX idx_name ON catalog ( lower(name) ) WHERE name!="" OR name IS NOT NULL;');
+$db->exec('CREATE INDEX idx_surname ON catalog ( lower(surname) ) WHERE surname!="" OR surname IS NOT NULL;');
+
+$db->exec('ALTER TABLE catalog ADD COLUMN wholename VARCHAR(765);');
+$db->exec('UPDATE wholename SET wholename = TRIM(surname||" "||name||" "||patronymic) FROM catalog;');
+$db->exec('CREATE INDEX idx_wholename ON catalog ( lower(wholename) ) ;');
+
+$db->exec('CREATE INDEX idx_lang ON catalog (language) WHERE language!="" OR language IS NOT NULL;');
+$db->exec('CREATE INDEX idx_title ON catalog ( lower(trim(title)) ) WHERE title!="" OR title IS NOT NULL;');
+$db->exec('CREATE INDEX idx_year ON catalog ( year ) WHERE year IS NOT NULL;');
+$db->exec('CREATE INDEX idx_series ON catalog ( lower(trim(series)) ) WHERE series!="" OR series IS NOT NULL;');
 
