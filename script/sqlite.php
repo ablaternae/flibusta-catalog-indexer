a9e7d62409ad92e3f4b45d6157db67e18e288cb4
d;) <ablaternae@users.noreply.github.com>
2022-04-04 14:12:14 +0300

    SQLITE3_DETERMINISTIC flag

--- script/sqlite.php
+++ script/sqlite.php
@@ -3,23 +3,23 @@ require (__DIR__.'/func.php');
 
 $db = new SQLite3('catalog.sqlite');
 
-$db->createFunction('lower', 'mb_strtolower');
-$db->createFunction('upper', 'mb_strtoupper');
+$db->createFunction('lower', 'mb_strtolower', 1, SQLITE3_DETERMINISTIC);
+$db->createFunction('upper', 'mb_strtoupper', 1, SQLITE3_DETERMINISTIC);
 //  $db->createFunction('ltrim', 'ltrim');
 //  $db->createFunction('rtrim', 'rtrim');
 //  $db->createFunction('trim', 'trim');
 $db->createFunction('explode', 'explode');
 $db->createFunction('implode', 'implode');
 
-$db->createFunction('metaphone', 'metaphone');
-$db->createFunction('translit', 'translit');
+$db->createFunction('metaphone', 'metaphone', -1, SQLITE3_DETERMINISTIC);
+$db->createFunction('translit', 'translit', 1, SQLITE3_DETERMINISTIC);
 
 //  CREATE TABLE catalog (surname varchar(255), name varchar(255), patronymic varchar(255), title text, subtitle text, language char(2), year int, series text, id integer);
 
 $db->exec('CREATE INDEX idx_name ON catalog ( lower(name) ) WHERE name!="" OR name IS NOT NULL;');
 $db->exec('CREATE INDEX idx_surname ON catalog ( lower(surname) ) WHERE surname!="" OR surname IS NOT NULL;');
 
-$db->exec('ALTER TABLE catalog ADD COLUMN wholename VARCHAR(765);');
+$db->exec('ALTER TABLE catalog ADD COLUMN wholename VARCHAR(128);');
 $db->exec('UPDATE wholename SET wholename = TRIM(surname||" "||name||" "||patronymic) FROM catalog;');
 $db->exec('CREATE INDEX idx_wholename ON catalog ( lower(wholename) ) ;');
 
